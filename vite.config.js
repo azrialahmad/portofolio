e292a29264f8d081e33ac815b72da4d67e5b742d
Azrial Ahmad Haidar Daffi Zuhri <81278995+azrialahmad@users.noreply.github.com>
2026-03-15 13:55:09 +0700

    change base domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// NOTE: Change `base` to '/' if deploying to username.github.io (user site)
-// or keep as '/portofolio/' if deploying to a project page (github.com/user/portofolio)
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio/',
+  base: '/',
 })
